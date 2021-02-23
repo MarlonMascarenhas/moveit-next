0ddc4b2632aeeacb6653e576db36edb9ad5a8c69
Marlon Mascarenhas <mmascarenhs@gmail.com>
2021-02-23 12:36:11 -0300

    Count down

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,23 @@
 import { Profile } from '../components/Profile';
 import { CompletedChallenges } from '../components/CompletedChallenges';
 import { ExperienceBar } from '../components/ExperienceBar';
+import Head from 'next/head';
 
 import styles from '../styles/pages/Home.module.css';
+import { Countdown } from '../components/Countdown';
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Inicio | move.it </title>
+      </Head>
       <ExperienceBar />
       <section>
         <div>
           <Profile />
           <CompletedChallenges />
+          <Countdown />
         </div>
         <div>
 
