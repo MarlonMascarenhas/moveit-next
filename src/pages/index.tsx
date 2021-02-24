09ac3a80a614436aeea59cd4c6e6982e5bceb5a2
Marlon Mascarenhas <mmascarenhs@gmail.com>
2021-02-24 15:05:12 -0300

    ChallengeBox estilizado

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
-import { Profile } from '../components/Profile';
-import { CompletedChallenges } from '../components/CompletedChallenges';
-import { ExperienceBar } from '../components/ExperienceBar';
 import Head from 'next/head';
 
+import { Profile } from '../components/Profile';
 import styles from '../styles/pages/Home.module.css';
 import { Countdown } from '../components/Countdown';
+import { ChallengeBox } from '../components/ChallengeBox';
+import { CompletedChallenges } from '../components/CompletedChallenges';
+import { ExperienceBar } from '../components/ExperienceBar';
 
 export default function Home() {
   return (
@@ -20,7 +21,7 @@ export default function Home() {
           <Countdown />
         </div>
         <div>
-
+          <ChallengeBox />
         </div>
       </section>
     </div>
